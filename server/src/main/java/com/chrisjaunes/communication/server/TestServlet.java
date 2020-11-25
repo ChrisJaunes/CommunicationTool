63fc1b5d05ce395f896aedb83b1237f83de2818c
ChrisJaunes <ChrisJaunes@gmail.com>
2020-11-26 06:03:08 +0800

    feat : 添加了Server端

---
+++ server/src/main/java/com/chrisjaunes/communication/server/TestServlet.java
@@ -0,0 +1,24 @@
+package com.chrisjaunes.communication.server;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "TestServlet", urlPatterns = "/TestServlet")
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8;");
+        String str = "hello，这是用于测试的页面";
+        System.out.println(str);
+        response.getWriter().println(str);
+    }
+}
